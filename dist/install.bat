81be6121fdcd64a1701fcfa17b4b23d05199b2bd
Joe Desimone <jdesimone@endgame.com>
2017-09-25 22:40:42 -0400

    fix install typo

--- dist/install.bat
+++ dist/install.bat
@@ -3,7 +3,7 @@ copy /y ClrHook32.dll c:\windows\syswow64\ClrHook32.dll
 
 
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v LoadAppInit_DLLs  /t REG_DWORD /f /d 0x1
-reg add "HKLM\Software\WOW6432Node\Microsoft\Windows NT\CurrentVersion\Windows" /v LoadAppInit_DLLs  /t /f REG_DWORD /d 0x1
+reg add "HKLM\Software\WOW6432Node\Microsoft\Windows NT\CurrentVersion\Windows" /v LoadAppInit_DLLs /t REG_DWORD /f /d 0x1
 
 reg add "HKLM\Software\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /t REG_SZ /f /d ClrHook64.dll
 reg add "HKLM\Software\WOW6432Node\Microsoft\Windows NT\CurrentVersion\Windows" /v AppInit_DLLs /t REG_SZ /f /d ClrHook32.dll
